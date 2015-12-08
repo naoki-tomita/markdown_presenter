2d596253c35b15215a561dfaa6d79b4df84bd738
naoki-tomita <naoki.tomita@jrits.ricoh.co.jp>
2015-12-08 17:01:48 +0900

    syntax highlight

--- app/helpers/upload_files_helper.rb
+++ app/helpers/upload_files_helper.rb
@@ -1,2 +1,10 @@
+require 'coderay'
 module UploadFilesHelper
+  def markdown_to_html(markdown)
+    GitHub::Markdown.to_html(markdown, :gfm) do |code, lang|
+      puts lang
+      puts CodeRay::FileType.fetch(lang, :text)
+      CodeRay.scan(code, CodeRay::FileType.fetch("hoge.#{lang}", :text)).div
+    end
+  end
 end
